0b290a2f498edea70a1cf35183ff6a0db6c57e3c
Igor Frolkin <frolkin_igor@mail.ru>
2012-05-25 16:10:12 +0700

    new version

--- String.cpp
+++ String.cpp
@@ -7,17 +7,25 @@ string str;
 void output_elements(int start, int end, int h)
 {
     int i;
-    if(h!=0)
-    {
-            for(i=start-1;((i>=0)&&(i<=end-1));i=i+h)
-            {
-                         cout<<str[i];
-            }
-            cout<<'\n';
+    int size=str.size();
+    if((start<=size)&&(start>=0)&&(end<=size)&&(end>=0))
+    {         
+             if(h!=0)
+             {
+                     for(i=start-1;((i>=0)&&(i<=end-1));i=i+h)
+                     {
+                                 cout<<str[i];
+                     }
+                     cout<<'\n';
+             }
+             else
+             {
+                     cout<<str[start-1]<<'\n';
+             }
     }
     else
     {
-            cout<<str[start-1]<<'\n';
+        cout<<"Required characters outside of the input string :(\n";
     }
 }
 
@@ -26,15 +34,19 @@ int main()
     cout<<"Please, enter your string: ";
     cin>>str;
     int size=str.size();
+    char c;
+    int i=0;
     while(size<5)
     {
-              cout<<"Length of your string must be more or equal 5, enter another string: ";
-              str.clear();
-              cin>>str;
+              cout<<"Length of your string must be more or equal 5, please enter more characters:";
+              c=getchar();
+              c=getchar();
+              str=str+c;
               size=str.size();
     }
+    cout<<str<<'\n';
     output_elements(3,size,0);
-    output_elements(size-1,sizeqw,0);
+    output_elements(size-1,size,0);
     output_elements(1,5,1);
     output_elements(1,size-2,1);
     output_elements(2,size,2);
@@ -42,6 +54,8 @@ int main()
     output_elements(size,size,-1);
     output_elements(size,size,-2);
     cout<<size<<'\n';
-    system("PAUSE");
+    cout<<"Press enter to exit the program..";
+    cin.get();
+    cin.get();
     return 0;
 }
